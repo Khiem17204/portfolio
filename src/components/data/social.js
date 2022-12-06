bd0cdf9fe207207cd153238fc236cad7a4fca0ed
Khiem <khiem17204@gmail.com>
2022-12-05 22:30:57 -0500

    fix contact component

--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,27 +1,13 @@
 export const SocialData=[
     {
-        platform:"Telegram",
-        icon: require("../../assets/icons/telegram.png"),
-        link:"https://web.telegram.org/k/",
-    },
-    {
         platform:"Github",
         icon: require("../../assets/icons/github.png"),
         link:"https://github.com/ThantZinPhyo-OP",
     },
     {
-        platform:"Youtube",
-        icon: require("../../assets/icons/youtube.png"),
-        link:"https://www.youtube.com/channel/UCUTEqqa6arBiK3cwDp_jAGA/featured",
-    },
-    {
-        platform:"Twitter",
-        icon: require("../../assets/icons/twitter.png"),
-        link: "https://twitter.com/ThantZinPhyoBR",
-    },
-    {
-        platform:"Facebook",
-        icon: require("../../assets/icons/facebook.png"),
-        link:"https://www.facebook.com/thantzin.phyo.18",
-    },
+        platform:"Linkedin",
+        icon: require("../../assets/icons/linkedin.png"),
+        link:"https://google.com"
+    }
+   
 ];
