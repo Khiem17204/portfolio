b5794797a200526476357a9a836c89817b17be52
Khiem <khiem17204@gmail.com>
2022-12-08 16:23:14 -0500

    finish contact form

--- src/components/body/contact/contactForm.js
+++ src/components/body/contact/contactForm.js
@@ -7,33 +7,63 @@ function ContactForm() {
       return <p>Thanks for joining!</p>;
   }
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
+    
+      <form onSubmit={handleSubmit} className='form'>
+      <div className='form-container'>
+        <label htmlFor="name" className='form-info'>
+            Name
+        </label>
+        <input
+            id="name"
+            type="name" 
+            name="name"
+            className='form-input-info'
+            placeholder='Khiem Le'
+        />
+        <ValidationError 
+            prefix="Name" 
+            field="name"
+            errors={state.errors}
+        />
+      </div>
+      <div className='form-container'>
+      <label htmlFor="email" className='form-info'>
         Email Address
       </label>
       <input
         id="email"
         type="email" 
         name="email"
+        className='form-input-info'
+        placeholder='abc@domain.xyz'
       />
       <ValidationError 
         prefix="Email" 
         field="email"
         errors={state.errors}
       />
+      </div>
+      <div className='form-container'>
+      <label htmlFor='message' className='form-info'>Message</label>  
       <textarea
+        className='form-input'
         id="message"
         name="message"
+        placeholder='I am open to any interesting work or collarboration opportunities'
       />
       <ValidationError 
         prefix="Message" 
         field="message"
         errors={state.errors}
       />
-      <button type="submit" disabled={state.submitting}>
+      </div>
+      <div className='form-container'>
+      <button type="submit" disabled={state.submitting} className='form-button'>
         Submit
       </button>
+      </div>
     </form>
+    
   );
 }
 export default ContactForm;
