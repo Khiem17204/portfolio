0e891c04c09819c59bb1cd491f1851ee45b00d82
Khiem <khiem17204@gmail.com>
2022-12-08 00:48:41 -0500

    fix

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -10,18 +10,18 @@ function Contact() {
     <div className="contact-container">
       <div className="contact-form">
         <p>Want to get in touch? Contact me on any of the platform</p>
-        <form name="contact-form" method="POST" data-netlify="true" className="form">
+        <form name="contact-form" method="post" data-netlify="true" className="form">
             <div className="form-container">
               <label htmlFor="name" className="form-info">Your Name:</label>
-              <input name='name' type="text" className="form-input" />
+              <input id="name" name='name' type="text" className="form-input" />
             </div>
             <div className="form-container">
               <label htmlFor="email" className="form-info">Your Email:</label>
-              <input name="email" type="email" className="form-input" />
+              <input id="email" name="email" type="email" className="form-input" />
             </div>
             <div className="form-container">
               <label htmlFor="message" className="form-info">Message:</label>
-              <textarea name="message" className="form-input"></textarea>
+              <textarea id="message"  name="message" className="form-input"></textarea>
             </div>
             <button type="submit" className="button" content>Send</button>
         </form>
