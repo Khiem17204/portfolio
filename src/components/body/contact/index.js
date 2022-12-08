e339da6c27a2bf2f1125f4ed6c1f6004eed257b3
Khiem <khiem17204@gmail.com>
2022-12-08 14:27:04 -0500

    test 3

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./contact.css";
 import Separator from "../../common/separator";
 import SocialContact from "../../common/social-contact";
+import contactForm from "./contactForm";
+
 function Contact() {
   return (
     <div className="contact">
@@ -10,27 +12,7 @@ function Contact() {
     <div className="contact-container">
       <div className="contact-form">
         <p>Want to get in touch? Contact me on any of the platform</p>
-        <form name="contact-form" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
+        <contactForm />
         <SocialContact />
         </div>
       </div>
