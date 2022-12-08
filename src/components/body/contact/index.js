5de7e48ccf244be79af4bc2c1fe22f91b38ad620
Khiem <khiem17204@gmail.com>
2022-12-08 14:14:14 -0500

    test form

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -10,21 +10,27 @@ function Contact() {
     <div className="contact-container">
       <div className="contact-form">
         <p>Want to get in touch? Contact me on any of the platform</p>
-        <form name="contact-form" method="post" data-netlify="true" className="form">
-            <div className="form-container">
-              <label htmlFor="name" className="form-info">Your Name:</label>
-              <input id="name" name='name' type="text" className="form-input" />
-            </div>
-            <div className="form-container">
-              <label htmlFor="email" className="form-info">Your Email:</label>
-              <input id="email" name="email" type="email" className="form-input" />
-            </div>
-            <div className="form-container">
-              <label htmlFor="message" className="form-info">Message:</label>
-              <textarea id="message"  name="message" className="form-input"></textarea>
-            </div>
-            <button type="submit" className="button" content>Send</button>
-        </form>
+        <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
         <SocialContact />
         </div>
       </div>
